0cab8d6ecab69f671567b056e9746b96628c31a1
NOE CAZAREZ CAMARGO <ncazarez@eca.uson.mx>
2017-03-11 06:52:32 -0700

    inicial

---
+++ PluginPoPupJs/PluginPoPupJs/index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>PlugIn PopUp y JS </title>
+        <style>
+            body{margin:0;padding:0;background:#222;color:#eee;}
+        </style>
+        <link href="css/estilo.css" rel="stylesheet" />
+    </head>
+<body>
+    <div id="nVentana" style="width:600px; margin-left:-300px; display: none;" class="lightBox-Container">
+        <div class="lightBox-Header">Titulo de la Ventana</div>
+        <div class="lightBox-Body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div class='ligthBox-Footer'>
+            <button class="btn-close" type="submit">Cerrar</button>
+        </div>
+    </div>
+    <button id="btnMostar">Mostar</button>
+
+    
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"/>
+    <script src="js/mce.lightbox.jquery.js"/>
+    <script>
+        
+        $(document).ready(function () {
+            $("#btnMostar").click(function () {
+                $.MCELightBox({
+                    id: 'mPrueba',
+                    width: 600,
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    title: 'Genial, soy un hermoso popup'
+                });
+            })            
+        })
+    </script>
+</body>
+</html>
---
+++ PluginPoPupJs/PluginPoPupJs/js/mce.lightbox.jquery.js
@@ -0,0 +1,30 @@
+﻿jQuery.MCELightBox = function (config) {
+
+    if (config.id == undefined) {
+        alert('Debe asignar un id para el popup');
+        return;
+    }
+
+    var _config = {
+        id: config.id,
+        width: (config.width != undefined ? config.width : 400),
+        image: (config.image != undefined ? config.image : false),
+        content: (config.content != undefined ? config.content : ''),
+        title: (config.title != undefined ? config.title : '')
+    };
+
+    var id = "#" + _config.id;
+
+    // Borramos el popup por si ya existìa
+    $(id).remove();
+
+    // Creamos el popup
+    var popup = '<div style="width:' + _config.width + 'px;margin-left:-' + (_config.width / 2) + 'px" class="lightBox-Container"><div class="lightBox-Header">' + _config.title + '</div><div class="lightBox-Body">' + (_config.image ? '<img class="lightBox-image" src="' + _config.content + '" />' : _config.content) + '</div><div class="ligthBox-Footer"><button class="btn-close" type="submit">Cerrar</button></div></div>';
+
+    $("body").prepend(popup);
+
+    // Anidamos el evento cerrar a nuestro boton
+    $('.btn-close', id).click(function () {
+        $(id).hide();
+    })
+}
